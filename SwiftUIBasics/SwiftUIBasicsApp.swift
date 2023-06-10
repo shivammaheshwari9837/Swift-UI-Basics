5d340602557ae1f9178c066205b911cec1078e8b
shivammaheshwari9837 <shivammaheshwari9837@gmail.com>
2023-06-10 22:13:05 +0530

    Initial Commit

---
+++ SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by Shivam Maheshwari on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
